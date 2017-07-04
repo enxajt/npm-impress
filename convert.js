d23867a87f502456ce3f2e6ef5b1f8b2eb45868b
enxajt <enxajt@gmail.com>
2017-07-04 13:43:40 +0900

    test separated convert function

--- convert.js
+++ convert.js
@@ -21,63 +21,57 @@ const md-it = require('markdown-it')({
   // If result starts with <pre... internal wrapper is skipped.
   highlight: function (/*str, lang*/) { return ''; }
 });
-  
-var moduleA = {
-  init: function(){
-    console.log('run init on moduleA');
-  }
-};
 
-  function convert(mdPath) {
-    var pages = fs.readFileSync(mdPath);
-  
-    pages = spawnSync('sed', ['-e', 's/^    /\\&nbsp;\\&nbsp;\\&nbsp;\\&nbsp;/g'], {input: pages}).stdout.toString();
-    pages = spawnSync('sed', ['-e', 's/^    /\\&nbsp;\\&nbsp;\\&nbsp;\\&nbsp;/g'], {input: pages}).stdout.toString();
-    pages = spawnSync('sed', ['-e', 's/^  /\\&nbsp;\\&nbsp;/g'], {input: pages}).stdout.toString();
-    pages = spawnSync('sed', ['-e', 's/^ /\\&nbsp;/g'], {input: pages}).stdout.toString();
-  
-    fs.writeFileSync('./hoge.json', JSON.stringify(md-it.parse(pages), null, '  ')); 
-  
-    pages = md-it.render(pages);
-    pages = spawnSync('perl', ['-0pe', 's/\n<h1><\\/h1>\n<h1>/\n<\\/div>\n\n<div class="step" >\n<h1>/mg'], {input: pages}).stdout.toString();
-    pages = spawnSync('perl', ['-0pe', 's/^<h1><\\/h1>\n<h1>/<div class="step" >\n<h1>/gm'], {input: pages}).stdout.toString();
-    pages = spawnSync('perl', ['-0pe', 's/<\\/h2>(.*?)(<\\/div>|<h1>|<h2>|<h3>)/<\\/h2>\n<section class="level-2" >$1<\\/section>\n$2/gs'], {input: pages}).stdout.toString();
-    pages = spawnSync('perl', ['-0pe', 's/<\\/h3>(.*?)(<\\/div>|<h1>|<h2>|<h3>|<h4>)/<\\/h3>\n<section class="level-3" >$1<\\/section>\n$2/gs'], {input: pages}).stdout.toString();
-    // -p print 必須 デフォルトでは1行ずつ-eの引数を評価する。つまりセパレータが\n。
-    // /g \nマッチ & -0セパレータがヌル文字(\0)ファイル全体を一度に読み込む
-    // /s .が改行を含む
-  
-    var html = fs.readFileSync('./ejs/index.html', 'utf8', function (err,data) {
-      if (err) { return console.log(err); }
-    });
-    var fileName = path.basename(mdPath);
-    var title = removeExtension(removeExtension(fileName));
-    var cssName = title+'.css';
-    html = ejs.render(html, {
-      title: title,
-      css: cssName,
-      pages: pages
-    });
-    fs.writeFile('./Notes/'+title+'.html', html, function(err) {
-        if(err) { return console.log(err); }
-    }); 
-  
-  //  console.log("start decktape(html>pdf)");
-  //  exec('./decktape-1.0.0/phantomjs ./decktape-1.0.0/decktape.js impress ./Notes/'+title+'.html ./Notes/'+title+'.pdf', (err, stdout, stderr) => {
-  //    if (err) { console.log(err); }
-  //    console.log(stdout);
-  //  });
-  
-    exec('[ -e ./Notes/'+cssName+' ] || cp ./Notes/impress/template.css ./Notes/'+cssName, (err, stdout, stderr) => {
-      if (err) { console.log(err); }
-      console.log(stdout);
-    });
-  
-    exec('cp -f '+mdPath+' ./Notes/'+fileName, (err, stdout, stderr) => {
-      if (err) { console.log(err); }
-      console.log(stdout);
-    });
-    return true;
-  }
+exports.main = function(mdPath) {
+  var pages = fs.readFileSync(mdPath);
 
-module.exports = moduleA;
+  pages = spawnSync('sed', ['-e', 's/^    /\\&nbsp;\\&nbsp;\\&nbsp;\\&nbsp;/g'], {input: pages}).stdout.toString();
+  pages = spawnSync('sed', ['-e', 's/^    /\\&nbsp;\\&nbsp;\\&nbsp;\\&nbsp;/g'], {input: pages}).stdout.toString();
+  pages = spawnSync('sed', ['-e', 's/^  /\\&nbsp;\\&nbsp;/g'], {input: pages}).stdout.toString();
+  pages = spawnSync('sed', ['-e', 's/^ /\\&nbsp;/g'], {input: pages}).stdout.toString();
+
+  fs.writeFileSync('./hoge.json', JSON.stringify(md-it.parse(pages), null, '  ')); 
+
+  pages = md-it.render(pages);
+  pages = spawnSync('perl', ['-0pe', 's/\n<h1><\\/h1>\n<h1>/\n<\\/div>\n\n<div class="step" >\n<h1>/mg'], {input: pages}).stdout.toString();
+  pages = spawnSync('perl', ['-0pe', 's/^<h1><\\/h1>\n<h1>/<div class="step" >\n<h1>/gm'], {input: pages}).stdout.toString();
+  pages = spawnSync('perl', ['-0pe', 's/<\\/h2>(.*?)(<\\/div>|<h1>|<h2>|<h3>)/<\\/h2>\n<section class="level-2" >$1<\\/section>\n$2/gs'], {input: pages}).stdout.toString();
+  pages = spawnSync('perl', ['-0pe', 's/<\\/h3>(.*?)(<\\/div>|<h1>|<h2>|<h3>|<h4>)/<\\/h3>\n<section class="level-3" >$1<\\/section>\n$2/gs'], {input: pages}).stdout.toString();
+  // -p print 必須 デフォルトでは1行ずつ-eの引数を評価する。つまりセパレータが\n。
+  // /g \nマッチ & -0セパレータがヌル文字(\0)ファイル全体を一度に読み込む
+  // /s .が改行を含む
+
+  var html = fs.readFileSync('./ejs/index.html', 'utf8', function (err,data) {
+    if (err) { return console.log(err); }
+  });
+  var fileName = path.basename(mdPath);
+  var title = removeExtension(removeExtension(fileName));
+  var cssName = title+'.css';
+  html = ejs.render(html, {
+    title: title,
+    css: cssName,
+    pages: pages
+  });
+
+  fs.writeFile('./Notes/'+title+'.html', html, function(err) {
+      if(err) { return console.log(err); }
+  }); 
+
+//  console.log("start decktape(html>pdf)");
+//  exec('./decktape-1.0.0/phantomjs ./decktape-1.0.0/decktape.js impress ./Notes/'+title+'.html ./Notes/'+title+'.pdf', (err, stdout, stderr) => {
+//    if (err) { console.log(err); }
+//    console.log(stdout);
+//  });
+
+  exec('[ -e ./Notes/'+cssName+' ] || cp ./Notes/impress/template.css ./Notes/'+cssName, (err, stdout, stderr) => {
+    if (err) { console.log(err); }
+    console.log(stdout);
+  });
+
+//  exec('cp -f '+mdPath+' ./Notes/'+fileName, (err, stdout, stderr) => {
+//    if (err) { console.log(err); }
+//    console.log(stdout);
+//  });
+
+  return true;
+}
--- impress.js
+++ impress.js
@@ -7,34 +7,35 @@ const execSync = require("child_process").execSync;
 const spawn = require('child_process').spawn;
 const spawnSync = require('child_process').spawnSync;
 
-const src_dir = "./Notes/";
-const dst_dir = "./md/";
-glob(src_dir+"/*.impress.md", function (er, files) {
-  files.forEach(function (_path, index, array) {
-    //var fn = _path.split(/\/(?=[^\/]+$)/)[1];
-    var fileName = path.basename(_path);
-    fs.createReadStream(_path).pipe(fs.createWriteStream(dst_dir + fileName));
-    console.log(index + ' copyied : ' + _path + ' to ' + dst_dir + fileName);
-  });
-})
+//const src_dir = "./Notes/";
+//const dst_dir = "./md/";
+//glob(src_dir+"/*.impress.md", function (er, files) {
+//  files.forEach(function (_path, index, array) {
+//    //var fn = _path.split(/\/(?=[^\/]+$)/)[1];
+//    var fileName = path.basename(_path);
+//    fs.createReadStream(_path).pipe(fs.createWriteStream(dst_dir + fileName));
+//    console.log(index + ' copyied : ' + _path + ' to ' + dst_dir + fileName);
+//  });
+//})
 
 const chokidar = require('chokidar')
+const convert = require('./convert.js');
 var watcher = chokidar.watch('./md', {
   ignored: /[\/\\]\./,
   persistent:true
+})
+watcher.on('ready', function() { console.log("start chokidar (file watching)"); })
+  .on('add', function(mdPath) {
+    console.log("chokidar is watching a file : " + mdPath);
+    if(convert.main(mdPath)) { console.log("converted : " + mdPath); }
   })
-watcher.on('ready', function() { console.log("start watching"); })
-  .on('add', function(path) {
-    console.log("added file -> " + path);
-    if(convert(path)) { console.log("converted -> " + path); }
+  .on('change', function(mdPath) {
+    console.log("chokidar found a modified file : " + mdPath);
+    if(convert.main(mdPath)) { console.log("converted : " + mdPath); }
   })
-  .on('change', function(path) {
-    console.log("modified file -> " + path);
-    if(convert(path)) { console.log("converted -> " + path); }
-  })
-  .on('addDir', function(path) { console.log("added dir -> " + path); })
-  .on('unlink', function(path) { console.log("deleted -> " + path); })
-  .on('unlinkDir', function(path) { console.log("deleted -> " + path); })
+  .on('addDir', function(mdPath) { console.log("added dir -> " + mdPath); })
+  .on('unlink', function(mdPath) { console.log("deleted -> " + mdPath); })
+  .on('unlinkDir', function(mdPath) { console.log("deleted -> " + mdPath); })
   .on('error', function(error) { console.log("error -> " + error); })
 
 function removeExtension(fileName) {
