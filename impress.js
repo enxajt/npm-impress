cc3647f8db0522a58a5ade566b38ccf413c3d2a1
enxajt <enxajt@gmail.com>
2017-07-12 09:57:44 +0900

    test

--- impress.js
+++ impress.js
@@ -28,10 +28,12 @@ function action(mdPath) {
   fs.writeFile('./impress-md/'+presn.title+'.html', presn.html, function(err) {
     if(err) { return console.log(err); }
   }); 
-  exec('[ -e ./impress-md/impress ] || cp ./impress ./impress-md/', (err, stdout, stderr) => {
+  console.log('made html');
+  exec('[ -e ./impress-md/impress ] || cp ./impress ./impress-md/impress', (err, stdout, stderr) => {
     if (err) { console.log(err); }
     console.log(stdout);
   });
+  console.log('cp js and css');
   exec('[ -e ./impress-md/'+presn.cssName+' ] || cp ./impress-md/impress/template.css ./impress-md/'+presn.cssName, (err, stdout, stderr) => {
     if (err) { console.log(err); }
     console.log(stdout);
