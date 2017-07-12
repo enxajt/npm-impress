5cffb2fd77423a66ca33078c519b64f8b37a6d4b
enxajt <enxajt@gmail.com>
2017-07-12 18:22:40 +0900

    test pages, md-it, marked

--- makeHTML.js
+++ makeHTML.js
@@ -2,6 +2,29 @@ const ejs = require('ejs');
 const exec = require('child_process').exec;
 const fs = require('fs');
 const path = require('path');
+const md_it = require('markdown-it')({
+  html:         true,        // Enable HTML tags in source
+  xhtmlOut:     false,        // Use '/' to close single tags (<br />).
+                              // This is only for full CommonMark compatibility.
+  breaks:       true,        // Convert '\n' in paragraphs into <br>
+  langPrefix:   'language-',  // CSS language prefix for fenced blocks. Can be
+                              // useful for external highlighters.
+  linkify:      false,        // Autoconvert URL-like text to links
+  // Enable some language-neutral replacement + quotes beautification
+  typographer:  false,
+
+  // Double + single quotes replacement pairs, when typographer enabled,
+  // and smartquotes on. Could be either a String or an Array.
+  //
+  // For example, you can use '«»„“' for Russian, '„“‚‘' for German,
+  // and ['«\xA0', '\xA0»', '‹\xA0', '\xA0›'] for French (including nbsp).
+  quotes: '“”‘’',
+
+  // Highlighter function. Should return escaped HTML,
+  // or '' if the source string is not changed and should be escaped externaly.
+  // If result starts with <pre... internal wrapper is skipped.
+  highlight: function (/*str, lang*/) { return ''; }
+});
 const spawnSync = require('child_process').spawnSync;
 var marked = require('marked');
 
@@ -13,10 +36,12 @@ exports.main = function(mdPath) {
   presn.pages = spawnSync('sed', ['-e', 's/^  /\\&nbsp;\\&nbsp;/g'], {input: presn.pages}).stdout.toString();
   presn.pages = spawnSync('sed', ['-e', 's/^ /\\&nbsp;/g'], {input: presn.pages}).stdout.toString();
 
+//  presn.pages = marked(presn.pages);
+//  presn.pages = md_it.render(presn.pages);
 //for test
-//  fs.writeFileSync('./hoge.json', JSON.stringify(md_it.parse(presn.pages), null, '  ')); 
-
-  presn.pages = marked(presn.pages);
+fs.writeFileSync('./impress-md/pages.test', JSON.stringify(presn.pages, null, '  ')); 
+fs.writeFileSync('./impress-md/md_it.test', JSON.stringify(md_it.parse(presn.pages), null, '  ')); 
+fs.writeFileSync('./impress-md/marked.test', JSON.stringify(marked(presn.pages), null, '  ')); 
   presn.pages = spawnSync('perl', ['-0pe', 's/\n<h1><\\/h1>\n<h1>/\n<\\/div>\n\n<div class="step" >\n<h1>/mg'], {input: presn.pages}).stdout.toString();
   presn.pages = spawnSync('perl', ['-0pe', 's/^<h1><\\/h1>\n<h1>/<div class="step" >\n<h1>/gm'], {input: presn.pages}).stdout.toString();
   presn.pages = spawnSync('perl', ['-0pe', 's/<\\/h2>(.*?)(<\\/div>|<h1>|<h2>|<h3>)/<\\/h2>\n<section class="level-2" >$1<\\/section>\n$2/gs'], {input: presn.pages}).stdout.toString();
