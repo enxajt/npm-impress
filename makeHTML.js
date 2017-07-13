a783d6c838b1640a0b5b387796166f6c4275c740
enxajt <enxajt@gmail.com>
2017-07-13 23:38:30 +0900

    try modifify <div class=step>

--- makeHTML.js
+++ makeHTML.js
@@ -35,10 +35,10 @@ exports.main = function(mdPath) {
   presn.pages = spawnSync('sed', ['-e', 's/^    /\\&nbsp;\\&nbsp;\\&nbsp;\\&nbsp;/g'], {input: presn.pages}).stdout.toString();
   presn.pages = spawnSync('sed', ['-e', 's/^  /\\&nbsp;\\&nbsp;/g'], {input: presn.pages}).stdout.toString();
   presn.pages = spawnSync('sed', ['-e', 's/^ /\\&nbsp;/g'], {input: presn.pages}).stdout.toString();
+  presn.pages = marked(presn.pages);
 //for test
 fs.writeFileSync('./impress-md/marked.test', JSON.stringify(presn.pages, null, '  ')); 
 
-  presn.pages = marked(presn.pages);
   presn.pages = spawnSync('perl', ['-0pe', 's/\n<h1 id=\"-\">---</h1>\n<h1>/\n<\\/div>\n\n<div class="step" >\n<h1>/mg'], {input: presn.pages}).stdout.toString();
   presn.pages = spawnSync('perl', ['-0pe', 's/^<h1 id=\"-\">---</h1>\n<h1>/<div class="step" >\n<h1>/gm'], {input: presn.pages}).stdout.toString();
   presn.pages = spawnSync('perl', ['-0pe', 's/<\\/h2>(.*?)(<\\/div>|<h1>|<h2>|<h3>)/<\\/h2>\n<section class="level-2" >$1<\\/section>\n$2/gs'], {input: presn.pages}).stdout.toString();
