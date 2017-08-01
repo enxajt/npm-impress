8f87d28c5655c957c233b0f7f11bf1cad13ca176
enxajt <enxajt@gmail.com>
2017-08-01 14:01:28 +0900

    test clean test-code for md-it

--- makeHTML.js
+++ makeHTML.js
@@ -5,30 +5,6 @@ const path = require('path');
 const spawnSync = require('child_process').spawnSync;
 const marked = require('marked');
 
-const md_it = require('markdown-it')({
-  html:         true,        // Enable HTML tags in source
-  xhtmlOut:     false,        // Use '/' to close single tags (<br />).
-                              // This is only for full CommonMark compatibility.
-  breaks:       true,        // Convert '\n' in paragraphs into <br>
-  langPrefix:   'language-',  // CSS language prefix for fenced blocks. Can be
-                              // useful for external highlighters.
-  linkify:      false,        // Autoconvert URL-like text to links
-  // Enable some language-neutral replacement + quotes beautification
-  typographer:  false,
-
-  // Double + single quotes replacement pairs, when typographer enabled,
-  // and smartquotes on. Could be either a String or an Array.
-  //
-  // For example, you can use '«»„“' for Russian, '„“‚‘' for German,
-  // and ['«\xA0', '\xA0»', '‹\xA0', '\xA0›'] for French (including nbsp).
-  quotes: '“”‘’',
-
-  // Highlighter function. Should return escaped HTML,
-  // or '' if the source string is not changed and should be escaped externaly.
-  // If result starts with <pre... internal wrapper is skipped.
-  highlight: function (/*str, lang*/) { return ''; }
-});
-
 exports.main = function(mdPath) {
   var presn = new Object();
   presn.pages = fs.readFileSync(mdPath);
@@ -36,17 +12,9 @@ exports.main = function(mdPath) {
   presn.pages = spawnSync('sed', ['-e', 's/^    /\\&nbsp;\\&nbsp;\\&nbsp;\\&nbsp;/g'], {input: presn.pages}).stdout.toString();
   presn.pages = spawnSync('sed', ['-e', 's/^  /\\&nbsp;\\&nbsp;/g'], {input: presn.pages}).stdout.toString();
   presn.pages = spawnSync('sed', ['-e', 's/^ /\\&nbsp;/g'], {input: presn.pages}).stdout.toString();
-
-fs.writeFileSync('./impress-md/md_it.test', md_it.render(presn.pages)); 
-
   presn.pages = marked(presn.pages);
-
-fs.writeFileSync('./impress-md/marked.test', presn.pages); 
-
   presn.pages = spawnSync('perl', ['-0pe', 's/\n<p>#<\\/p>\n<h1/\n<\\/div>\n\n<div class="step" >\n<h1/mg'], {input: presn.pages}).stdout.toString();
-
   presn.pages = spawnSync('perl', ['-0pe', 's/^<p>#<\\/p>\n<h1/<div class="step" >\n<h1/gm'], {input: presn.pages}).stdout.toString();
-
   presn.pages = spawnSync('perl', ['-0pe', 's/<\\/h2>(.*?)(<\\/div>|<h1>|<h2>|<h3>)/<\\/h2>\n<section class="level-2" >$1<\\/section>\n$2/gs'], {input: presn.pages}).stdout.toString();
   presn.pages = spawnSync('perl', ['-0pe', 's/<\\/h3>(.*?)(<\\/div>|<h1>|<h2>|<h3>|<h4>)/<\\/h3>\n<section class="level-3" >$1<\\/section>\n$2/gs'], {input: presn.pages}).stdout.toString();
   // -p print 必須 デフォルトでは1行ずつ-eの引数を評価する。つまりセパレータが\n。
